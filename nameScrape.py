985ddf74134774c55168abb2f23654103cc8eb10
Raymond Sangalang <xxraymondxx55@yahoo.com>
2020-01-31 21:26:26 -0800

    First Commit

--- nameScrape.py
+++ nameScrape.py
@@ -1,39 +1,60 @@
 # CIS41A-lab5 name.py
 # Raymond Sangalang
-# back end to application: reads and stores data from the file and allows searches for dat
-
-import re
-import requests
+# back end to application: reads and stores data from the file and allows searches for data
+import re, requests, sqlite3
 from requests.exceptions import HTTPError
 from bs4 import BeautifulSoup
 
-pattern = re.compile("(^[>](.+)</a|/a>(.+)<|([a-zA-Z\s]+))")
+
+''' creating a database through sqlite3 into file _nbaPlayer.db '''
+'''
+_link= sqlite3.connect('_nbaPlayer.db')
+_cur= _link.cursor()
+
+_cur.execute("""CREATE TABLE RECORDS()""")
+_link.close()
+'''
+str_pattern= re.compile("[^\s\d,'/][a-zA-Z\.\-\s]+")
+
 class Name:
 
-    
     def __init__(self, _fileName=''):
         ''' Constructor- requests(interface)  '''
+        
+        
         self.players = {}
-        #if _fileName is None:
-            #return
-    
-        content= requests.get(_fileName)                    # creates HTML page object from request
+        self.listOfNames, self.listOfStats = [], []      # listOfNames --> player name, team, and position
+                                                         # listOfStats --> rank, game played, min/game, off-reb/game, def-reb/game, reb/game, wins
+
+        content= requests.get(_fileName)                 # creates HTML page object from request
         content.raise_for_status()
         
-        soup= BeautifulSoup(content.text,'html.parser')     # flexible HTML parser-BeautifulSoup-gets python byte string from request obj
+        soup= BeautifulSoup(content.text,'html.parser')              # flexible HTML parser-BeautifulSoup-gets python byte string from request obj
         table= soup.find('table', {'class': 'tablehead'})
 
         for tr in table.find_all('tr', {'class': self.match_tag}):
-            listOfStats= re.findall('\d+(?:\.\d+)?', str([td.text for td in tr.find_all('td')]))
-            listOfNames= re.findall("[^\s,'][a-zA-Z\s]+", str([td.text for td in tr.find_all('td')]))
-            self.players[listOfNames[0]]= tuple(listOfNames[1:]+listOfStats)
+            self.listOfStats= re.findall('\d+(?:\.\d+)?', str([td.text for td in tr.find_all('td')]))
+            self.listOfNames= re.findall(str_pattern, str([td.text for td in tr.find_all('td')]))
+            self.players[self.listOfNames[0]]= self.listOfNames[1:]+self.listOfStats
 
-            print(f"{', '.join([str(i) for i in listOfNames]):30s} ==> {', '.join([str(i) for i in listOfStats])}\n")
-        if len(self.players) == 0: return None
+            print(f"{', '.join([str(i) for i in self.listOfNames]):30s} ==> {', '.join([str(i) for i in self.listOfStats])}\n")
+            
+        '''print(f"Name {' '*25} Stats")
+        for k, v in self.players.items():
+            print(f"{k:30s} {', '.join(list(v))}")'''
+        '''
+        if len(self.listOfNames) != 0:
+            print(_fileName)
+            for _init, _stat in zip(self.listOfNames, self.listOfStats):
+                print(f"{', '.join(_init.split()):30s} ==> {', '.join([str(_s) for _s in _stat])}")
+            #print(f"{', '.join([str(i) for i in self.listOfNames]):30s} ==> {', '.join([str(i) for i in self.listOfStats])}\n")'''
+ 
             
             
     def match_tag(self, _tag):
         return True if _tag and (_tag.startswith('oddrow') or _tag.startswith('evenrow')) else False
+    
+    def __len__(self): return len(self.listOfNames)
         
 
 
--- webLab.py
+++ webLab.py
@@ -2,63 +2,54 @@
 #         - The program implements UI(user-interface) to communicate with user and retrives data from the Names(back-end database) to
 #           compile data from a file and allow searching of data
 from nameScrape import Name, re, requests
-import os
+#import os
 
 
 class UI:
     ''' UI(user-interface)- implements front-end app: constructor for file, and run method to utilize methods
-                            from Name and catches exceptions to acquire data validation.                     '''
+                            from player Name and catches exceptions to acquire data validation.                     '''
 
-    curr_year= "http://www.espn.com/nba/statistics/rpm"
-    __STARTFILE= "http://www.espn.com/nba/statistics/rpm/_/year/"
+    # WebScrape/Crawl into espn files
+    __STARTFILE, curr_year= ("http://www.espn.com/nba/statistics/rpm/_/year/", "http://www.espn.com/nba/statistics/rpm")
 
     def __init__(self):
         ''' constructor: keep looping until file is able to be read in, then create Name object'''
+        
         for num_year in range(2014, 2021):
+            ''' Collecting data in years starting from 2014 to 2020 '''
+            
             print(f"\n\n\t\tYEAR: {num_year-1}-{num_year}\n\t\t{'-'*15}\n")
             filetouse= self.__STARTFILE+str(num_year) if num_year != 2021 else self.curr_year
             searchWeb= filetouse
             
-            page_count=1
-            status = True
-            while status: 
-                print(searchWeb)
+            page_count=1                                        # start of page count
+            while True: 
                 ''' loops until valid file name is read, and if user just entered, then utilizes default file name'''
-                #try:   #searchWeb= str(input(f"\n\tSearching Data\n\t{'-'*14}\nEnter a file <Enter key for default>: "))
-
-                        
-                    
-                #searchWeb= __filetouse
+                print(searchWeb)
                 _data= Name(searchWeb)
-                page_count+=1 
+                page_count+=1                                   # increment to next page of data
+                
+                if len(_data) == 0: break                       # if page has no data, break out of loop
+                else: 
+                    self._dataNames = _data                
+                
                 if page_count >= 2 and num_year == 2021:
                     searchWeb= filetouse + f"/_/page/{page_count}"
                 elif page_count >= 2: 
                     searchWeb= filetouse + f"/page/{page_count}"
-                
-    
-                #if searchWeb == "" : self._dataNames= Name(self.__DEFAULTFILE)
-                #elif searchWeb: self._dataNames= Name(searchWeb)
-                if _data == None: status = False
-                else:
-                    self._dataNames = _data
-                
-                                                                  # increment to next page of data
-    
-                #break    
-                '''
-                except FileNotFoundError: print("ERROR: opening input file")
+                                                             
+
+                ''' try: except FileNotFoundError: print("ERROR: opening input file")
                 except requests.exceptions.HTTPError as ht: print("HTTP ERROR:", ht)
-                except requests.exceptions.ConnectionError as ct: print("Connection ERROR:", ct)
-                '''
-                #break
+                except requests.exceptions.ConnectionError as ct: print("Connection ERROR:", ct) '''
+
 
     def getData(self): return self._dataNames  # getter to Name class
+    
 
-
-
+    
+    
 
 if __name__ == "__main__":
-
     app= UI()
-    app.run()
+
