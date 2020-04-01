a88c4028f90cb8c7b147b00c455e209f085c4876
raymond-sangalang <59466702+raymond-sangalang@users.noreply.github.com>
2020-04-01 16:59:10 -0700

    Update playerDB.py

--- playerDB.py
+++ playerDB.py
@@ -3,6 +3,7 @@ import sqlite3
 from sqlite3 import Error
 
 def connect_SQL():
+    ''' Connect to SQL database into _nbaPlayer.db '''
     try:
         return sqlite3.connect('_nbaPlayer.db')
     except Error as e:
@@ -17,7 +18,7 @@ def createTables(_conn):
     _cur= _conn.cursor()
     _cur.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='Player' ''')
     
-    if _cur.fetchone()[0] != 0:
+    if _cur.fetchone()[0] != 0:                    # condition: if tables exist return, else move on to creating them 
         return   
     
     _cur.execute("""CREATE TABLE YearOfPlayer
@@ -36,24 +37,34 @@ def createTables(_conn):
     _conn.commit() # save changes 
 
 
-def add_playersYear(player_year, team, rank):
+def add_playersYear(_cur, player_year, team, rank):
     ''' insert function to SQL commands, inputting a players year(TABLE) by rank and team '''
     
     _cur.execute("""INSERT INTO YearOfPlayer (player_year, team, rank)
                     VALUES(?,?,?)""", (player_year, team, rank))
-    _conn.commit() # save changes     
+    #_conn.commit() # save changes     
     
-def add_stats(game_played, min_per_game, off_reb_per_game, def_reb_per_game, reb_per_game, wins):
+def add_stats(_cur, game_played, min_per_game, off_reb_per_game, def_reb_per_game, reb_per_game, wins):
     ''' Insert into inputting a players stat table: games played, mins/game, reb/game and wins by plus/minus scores '''
     
     _cur.execute("""INSERT INTO StatsOfPlayer (game_played, min_per_game, off_reb_per_game, 
                                                def_reb_per_game, reb_per_game, wins)
-            VALUES(?,?,?,?,?)""", (game_played, min_per_game, off_reb_per_game, def_reb_per_game, reb_per_game, wins))
-    _conn.commit() # save changes 
+            VALUES(?,?,?,?,?,?)""", (game_played, min_per_game, off_reb_per_game, def_reb_per_game, reb_per_game, wins))
+    #_conn.commit() # save changes 
     
-def add_Player(name, year, key):
+def add_Player(_cur, name, year, key=0):
     ''' add_Player: inputting a new players(objects for data) with attributes by name and years played '''
     
     _cur.execute("""INSERT INTO Player (name, year, key)
                     VALUES(?,?,?)""", (name, year, key))
-    _conn.commit() # save changes   
+    #_conn.commit() # save changes
+    
+def add_to_tables(_conn, playerYear, player_stats, playerObj):
+    
+    ''' add_to_tables: function taking 3 list arguments to insert into all three tables
+                       as a function decorator'''
+    _cur= _conn.cursor()
+    add_playersYear(_cur, *playerYear)
+    add_stats(_cur, *player_stats)
+    add_Player(_cur, *playerObj)         #still need to create key
+    _conn.commit()
