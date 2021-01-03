b3f188d80a7923f99d72f4450e0ece2311535418
raymond-sangalang <59466702+raymond-sangalang@users.noreply.github.com>
2021-01-02 21:35:28 -0800

    Update Team.py

--- Team.py
+++ Team.py
@@ -1,89 +1,100 @@
 """ Team will acquire a projection of players compared to another instantiated Team
      Note: number of players must meet to compare                                     """
-from Player import Player
+from player import Player
 
 
 class Team:
 
-    def __init__(self, numberOfSeats=0):
+    def __init__(self, name, numberOfYears=0, spots=None):
+        self.name = name
+        self.numberOfYears = numberOfYears               # lowest years played for all players on roster
+        if spots is None:
+            self.spots = []  # list of all players on team
+        else:
+            self.spots = list(spots)
 
-        self.numberOfSeats = numberOfSeats
-        self.spots = []  # list of all players on team
+    def getNumberOfYears(self):
+        return self.numberOfYears
 
-    def getNumberOfSeats(self):
-        return self.numberOfSeats
+    def setNumberOfYears(self, numberOfYears):
+        self.numberOfYears = numberOfYears
+
+    def getFront(self):
+        return self.spots[0]
+
 
     def getRoster(self):
         return self.spots
 
     def addPlayer(self, player=None):
         """ addPlayer - fills spots on team and takes the shortest amount of years """
+
+        if not isinstance(player, Player) or any(i for i in self.spots if i == player):
+            print("Invalid Entry: Must hold player object not in team")
+            return False
+
         self.getRoster().append(player)
         self.minimalYears()
+        return True
+
 
     def removePlayer(self, delName):
 
         if len(self) == 0:
             print("There is no spots to remove off roster")
-            return
-        index = 0
-        while index < len(self):
-            if self.spots[index].toString() == delName:
-                self.getRoster().pop(index)
-                self.minimalYears()
-                return
-            index += 1
+        else:
 
-        print(f"Could Not Find {delName} on team ")
+            index = 0
+            while index < len(self):
+
+                if self.spots[index].toString() == delName:
+                    self.getRoster().pop(index)
+                    self.minimalYears()
+                    return
+                index += 1
+
+            print(f"Could Not Find {delName} on team ")
 
     def minimalYears(self):
 
-        if len(self) <= 1:
-            self.numberOfSeats = self.getRoster()[0].yearsInDB()
+        if len(self) > 1:
+            lowestYears = min([seat.yearsInDB() for seat in self.getRoster()])
         else:
-            self.numberOfSeats = min([seat.yearsInDB() for seat in self.getRoster()])
+            lowestYears = self.getFront().yearsInDB()
+
+        self.setNumberOfYears(lowestYears)
+
+    def getAverage(self, choice=0):
+
+        if choice not in (1, 2, 3):
+            print("ERROR: INVALID CHOICE!!!")
+
+        else:
+            print("\nAVERAGE WINS")
+            for i in self.getRoster():
+                print(f"\t{i.getName}: {i.avgChoice(self.getNumberOfYears(), choice):.2f}")
+
 
     def print(self):
-        for spot in self.getRoster():
-            spot.printStats()
+        for sp in self.getRoster():
+            sp.printStats()
+
 
     def printRoster(self):
-        for i, name in enumerate(self.getRoster()):
-            print(i + 1, str(name))
+        print(f"{self.name}'s Lineup\n")
+        for index, name in enumerate(self.getRoster()):
+            print("\t", index + 1, str(name))
+
+
+    def __str__(self):
+        ls = [f"\nTeam name: {self.name}", "Players:"]
+        ls.extend("\t" + str(player) for player in self)
+        return "\n".join(ls)
+
 
     def __len__(self):
         return len(self.spots)
 
+    def __iter__(self):
+        return iter(self.spots)
 
-if __name__ == "__main__":
-    """ check new classes to be robust into the system """
-
-    playersName = "Stephen Curry"
-    yearsPlayed = range(2014, 2021)
-    minPerGamePlayed = [36.5, 32.7, 34.2, 33.4, 32.0, 33.8, 27.8]
-    total_rpgPlayed = [6.53, 10.92, 11.37, 7.96, 6.2, 7.6, 4.72]
-    winsPlayed = [18.76, 25.77, 27.01, 19.03, 9.4, 15.69, 0.73]
-
-    player1 = Player(playersName, yearsPlayed, minPerGamePlayed, total_rpgPlayed, winsPlayed)
-
-    playersName = "Klay Thompson"
-    yearsPlayed = range(2014, 2020)
-    minPerGamePlayed = [35.4, 31.9, 33.3, 34.0, 34.3, 34.0]
-    total_rpgPlayed = [3.54, 4.13, 3.87, 3.98, 3.46, 2.39]
-    winsPlayed = [12.75, 12.03, 11.92, 11.01, 9.01, 7.07]
-
-    player2 = Player(playersName, yearsPlayed, minPerGamePlayed, total_rpgPlayed, winsPlayed)
-
-    team1 = Team()
-    team1.addPlayer(player1)
-    team1.addPlayer(player2)
-
-
-    team1.printRoster()
-
-    minYear = team1.getNumberOfSeats()
-    print("The minimal number of years for all players:", minYear,
-          f"\nThe team has {len(team1)} members")
-
-    print(f"\n\t{player1.getName()} average wins: {player1.avgChoice(minYear):.2f}")
-    print(f"\n\t{player2.getName()} average wins: {player2.avgChoice(minYear):.2f}")
